657f953100c15e0f9426228acca2b7e4c6afe81d
Prantik <prantik3332@gmail.com>
2026-08-19 12:50:26 +0600

    Refactor User class formatting and structure

--- src/main/java/com/prantik/nodeshare/models/User.java
+++ src/main/java/com/prantik/nodeshare/models/User.java
@@ -18,7 +18,6 @@ public class User {
         this.isOnline = true;
     }
     
-    // Getters and Setters
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
     
